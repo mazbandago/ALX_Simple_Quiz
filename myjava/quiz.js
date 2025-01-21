ccfe19055432e96cc86ac4f3beca2a7a46a40d12
Mustapha Abdulai <mazbandago@gmail.com>
2025-01-21 14:11:03 +0000

    created index, styles and quiz.js

---
+++ myjava/quiz.js
@@ -0,0 +1,9 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+    return correctAnswer;
+}
+
+let results = checkAnswer()
+
+const pick = document.querySelector('quiz');
+pick.getAttribute('name');
