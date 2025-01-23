e7d7a4268590ef0e69eb866640d81130c4cc9f30
Mustapha Abdulai <mazbandago@gmail.com>
2025-01-23 01:53:26 +0000

    update javafile

--- myjava/quiz.js
+++ myjava/quiz.js
@@ -1,9 +1,24 @@
 function checkAnswer() {
-    let correctAnswer = "4";
-    return correctAnswer;
+let correctAnswer = "4";
+return correctAnswer
 }
 
 let results = checkAnswer()
 
-const pick = document.querySelector('quiz');
-pick.getAttribute('name');
+const pick = document.querySelector('input');
+
+pick.getAttribute('value');
+
+const userAnswer = pick.getAttribute('value');
+
+if(userAnswer===results){
+    const feedBack = document.querySelector('#feedback');
+    feedBack.textContent = "Correct! Well done.";
+} else {
+    feedBack.textContent = "That's incorrect. Try again!";
+};
+
+// const submitAnswer = document.getElementById('#submit-answer');
+// submitAnswer.addEventListener('click', function checkAnswer{  
+//     alert('This is the right answer');
+// });
