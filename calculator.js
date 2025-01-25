f6e32e6000183449c397b9923dc698ef99a54f1e
Mustapha Abdulai <mazbandago@gmail.com>
2025-01-25 05:41:33 +0000

    update calculator

--- calculator.js
+++ calculator.js
@@ -2,13 +2,57 @@ const number1 = document.querySelector('#number1');
 number1.getAttribute('number');
 
 const number2 = document.querySelector('#number2');
-number2.getAttribute('number2');
+number2.getAttribute('number');
 
 function add(number1, number2){ 
     return number1 + number2;
 };
 
-results = add();
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click',function(){   
+    number1 = parseFloat(document.getElementById('number1').value) || 0;
+    number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = 'result';
 
-const Add = document.querySelector('#add');
-add.
+});
+
+function subtract(number1, number2){ 
+    return number1 - number2;
+};
+
+const subtractBtn = document.getElementById('subtract');
+subtractBtn.addEventListener('click',function(){   
+    number1 = parseFloat(document.getElementById('number1').value) || 0;
+    number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = subtract(number1, number2);
+    document.getElementById('calculation-result').textContent = 'result';
+
+});
+
+function multiply(number1, number2){ 
+    return number1 * number2;
+};
+
+const multiplyBtn = document.getElementById('multiply');
+multiplyBtn.addEventListener('click',function(){   
+    number1 = parseFloat(document.getElementById('number1').value) || 0;
+    number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = 'result';
+
+});
+
+function divide(number1, number2){ 
+    return number1 / number2;
+};
+
+const divideBtn = document.getElementById('divide');
+addBtn.addEventListener('click',function(){   
+    number1 = parseFloat(document.getElementById('number1').value) || 0;
+    number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(number1, number2);
+    document.getElementById('calculation-result').textContent = 'result';
+
+});
+
