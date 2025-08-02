c7c0ef2841eeaa2f169e143e84fff670f4ac81a2
Mustapha Abdulai <mazbandago@gmail.com>
2025-08-02 09:27:23 +0000

    ainclude userAnswer===CheckAnswer

--- quiz.js
+++ quiz.js
@@ -1,14 +1,18 @@
 function checkAnswer() {
 const correctAnswer = '4';
-const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+const radioSelected = document.querySelector('input[name="quiz"]:checked');
 
-if (userAnswer===correctAnswer){
+if (!radioSelected){
     const feedBack = document.querySelector('#feedback');
-    feedBack.textContent = "Correct! Well done.";
-} else {
     feedBack.textContent = "That's incorrect. Try again!";
-};
-
+    return
+} 
+const userAnswer = radioSelected.value;
+if(userAnswer===checkAnswer){
+    document.querySelector('#feedback').textContent = 'Correct! Well done.';
+} else {
+    document.querySelector('#feedback').textContent = "That's incorrect. Try again!";
+}
+}
 const submitAnswer = document.getElementById('submit-answer');
 submitAnswer.addEventListener('click', checkAnswer);
-};
